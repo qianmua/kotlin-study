11ff56b4d8f55e7f73d48b92480e7bb1c7898c79
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-03-31 22:38:29 +0800

    uuid

--- src/main/kotlin/pres/hjc/kotlinspringboot/tools/PublicToolsUtils.kt
+++ src/main/kotlin/pres/hjc/kotlinspringboot/tools/PublicToolsUtils.kt
@@ -137,6 +137,8 @@ object PublicToolsUtils {
     /**
      * uuid
      */
+    fun
+
 
 
     /**
---
+++ src/main/kotlin/pres/hjc/kotlinspringboot/typeset/DayGetCont.kt
@@ -0,0 +1,15 @@
+package pres.hjc.kotlinspringboot.typeset
+
+/**
+Created by IntelliJ IDEA.
+@author HJC
+@date 2020/3/31
+@time 22:34
+@version 1.0
+To change this template use File | Settings | File Templates.
+ */
+enum class DayGetCont {
+
+    MON,TUE,WED,THU,FRI,SAT,SUN
+
+}
