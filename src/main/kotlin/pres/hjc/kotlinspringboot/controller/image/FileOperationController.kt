28e4a4b3ef3d581c3f81cc6c34cac3525104278f
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-04-01 19:27:21 +0800

    file upload

--- src/main/kotlin/pres/hjc/kotlinspringboot/controller/image/FileOperationController.kt
+++ src/main/kotlin/pres/hjc/kotlinspringboot/controller/image/FileOperationController.kt
@@ -2,10 +2,12 @@ package pres.hjc.kotlinspringboot.controller.image
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.multipart.MultipartFile
 import pres.hjc.kotlinspringboot.config.quniu.FileUploadConfig
+import pres.hjc.kotlinspringboot.tools.ConstantUtils
 import pres.hjc.kotlinspringboot.tools.PublicToolsUtils
 
 /**
@@ -22,9 +24,15 @@ class FileOperationController {
     @Autowired
     private lateinit var fileUploadConfig:FileUploadConfig
 
+    companion object{
+        private const val suf = ConstantUtils.suffix
+    }
     @PostMapping("/uimg")
     fun fileUploadQiniu(file:MultipartFile):String{
         val upFile = PublicToolsUtils.UploadFileQiniu(fileUploadConfig.getQiniu())
         return upFile.upLoadFile(file)
     }
+    @GetMapping("/op$suf")
+    fun pageop():String = "pictures/open-pictures"
+
 }
