921c4c1354b560b6e9150609166a0f64541e1845
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-04-01 16:04:18 +0800

    uuid

---
+++ src/main/kotlin/pres/hjc/kotlinspringboot/controller/admin/FileUploadController.kt
@@ -0,0 +1,18 @@
+package pres.hjc.kotlinspringboot.controller.admin
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+/**
+Created by IntelliJ IDEA.
+@author HJC
+@date 2020/4/1
+@time 10:08
+@version 1.0
+To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequestMapping("/")
+class FileUploadController {
+
+}
